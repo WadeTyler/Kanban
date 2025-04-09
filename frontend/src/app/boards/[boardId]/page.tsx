b3354d32ce93728f99ef13fc582c5a05ba16f6b4
Tyler Wade <vaxonreal@gmail.com>
2025-04-09 17:14:29 -0400

    Added functionality to change board list positions locally.

--- frontend/src/app/boards/[boardId]/page.tsx
+++ frontend/src/app/boards/[boardId]/page.tsx
@@ -2,13 +2,13 @@
 import React, {useEffect, useState} from 'react';
 import useBoardStore from "@/stores/board.store";
 import useAuthStore from "@/stores/auth.store";
-import {Board} from "@/types/board.types";
+import {Board, BoardList as BoardList_Type} from "@/types/board.types";
 import {useParams} from "next/navigation";
 import AuthProvider from "@/providers/AuthProvider";
 import LoadingSpinner from "@/components/LoadingSpinner";
 import {RiSettings2Line} from "@remixicon/react";
 import Label from "@/components/Label";
-import {BoardList, CreateNewBoardList} from "@/components/board/BoardList";
+import {BoardList, BoardListOverlay, CreateNewBoardList} from "@/components/board/BoardList";
 import {useWebSocketStore} from "@/stores/websocket.store";
 import ConnectedUsers from "@/components/board/ConnectedUsers";
 import BoardSettings from "@/components/board/BoardSettings";
@@ -16,6 +16,15 @@ import {useBoardUIStore} from "@/stores/board-ui.store";
 import ScreenPanelOverlay from "@/components/ScreenPanelOverlay";
 import ListItemSettings from "@/components/board/list-item/ListItemSettings";
 import EditStatusTypes from "@/components/board/EditStatusTypes";
+import {
+  closestCenter,
+  DndContext,
+  DragOverlay,
+  PointerSensor,
+  useSensor,
+  useSensors
+} from "@dnd-kit/core";
+import {arrayMove, horizontalListSortingStrategy, SortableContext} from "@dnd-kit/sortable";
 
 const Page = () => {
   // Nav
@@ -129,6 +138,51 @@ const Page = () => {
     }
   }, [updatedBoard]);
 
+
+  ////////////////////////// DRAGGABLE //////////////////////////
+
+  const [activeBoardList, setActiveBoardList] = useState<BoardList_Type | null>(null);
+
+  const sensors = useSensors(
+    useSensor(PointerSensor)
+  );
+
+  const handleDragStart = (event: any) => {
+    setActiveBoardList(board?.lists?.find(list => list.boardListId === event.active.id) || null);
+  }
+
+  const handleDragEnd = (event: any) => {
+
+    const {active, over} = event;
+
+    if (active.id !== over.id && board?.lists) {
+      const boardLists = board.lists;
+
+      const oldIndex = boardLists.indexOf(boardLists.find(list => list.boardListId === active.id) as BoardList_Type);
+      const newIndex = boardLists.indexOf(boardLists.find(list => list.boardListId === over.id) as BoardList_Type);
+
+      // Move the list
+      const updatedBoardList = arrayMove<BoardList_Type>(boardLists, oldIndex, newIndex).map((list, index) => {
+        return {
+          ...list,
+          position: index,  // Update the position of each list
+        }
+      });
+
+      console.log("List: ", updatedBoardList);
+
+      setBoard({
+        ...board,
+        lists: updatedBoardList
+      });
+
+      setActiveBoardList(null);
+
+
+      // TODO: Send the updated board list to the server
+    }
+  }
+
   ////////////////////////// RETURNS //////////////////////////
 
   if (isLoadingBoard) {
@@ -141,55 +195,80 @@ const Page = () => {
 
   return (
     <AuthProvider authRequired={true}>
-      <div className="w-full h-screen page-padding">
-        {loadBoardError && <p className="text-danger text-center">{loadBoardError}</p>}
+      <DndContext
+        sensors={sensors}
+        collisionDetection={closestCenter}
+        onDragStart={handleDragStart}
+        onDragEnd={handleDragEnd}
+      >
+        <div className="w-full h-screen page-padding">
+          {loadBoardError && <p className="text-danger text-center">{loadBoardError}</p>}
 
-        {board && (
-          <div className="w-full h-full flex flex-col gap-4">
-            <div className="flex items-center justify-between gap-4 w-full">
-              <h1 className="text-xl font-semibold text-accent">{board.name}</h1>
-              {/* Action Btns */}
-              <div className="flex items-center gap-4">
+          {board && (
+            <div className="w-full h-full flex flex-col gap-4">
+              <div className="flex items-center justify-between gap-4 w-full">
+                <h1 className="text-xl font-semibold text-accent">{board.name}</h1>
+                {/* Action Btns */}
+                <div className="flex items-center gap-4">
 
-                <ConnectedUsers connectedUsers={connectedUsers} boardOwner={board.owner} />
+                  <ConnectedUsers connectedUsers={connectedUsers} boardOwner={board.owner}/>
 
-                <div className="relative group h-fit" >
-                  <RiSettings2Line className="hover-btn size-8" onClick={() => setIsShowingSettings(prev => !prev)}/>
-                  {!isShowingSettings && <Label text={"Settings"}/>}
-                  {isShowingSettings && board && (
-                    <BoardSettings board={board} editStatusTypes={() => setIsEditingStatusTypes(true)}/>)
-                  }
+                  <div className="relative group h-fit">
+                    <RiSettings2Line className="hover-btn size-8"
+                                     onClick={() => setIsShowingSettings(prev => !prev)}/>
+                    {!isShowingSettings && <Label text={"Settings"}/>}
+                    {isShowingSettings && board && (
+                      <BoardSettings board={board} editStatusTypes={() => setIsEditingStatusTypes(true)}/>)
+                    }
+                  </div>
+
                 </div>
-
               </div>
+              <hr className="text-secondary"/>
+              <div className="w-full h-full flex gap-8 overflow-x-scroll overflow-y-hidden">
+                {board.lists && (
+                  <SortableContext
+                    items={board.lists?.map(list => (list.boardListId))}
+                    strategy={horizontalListSortingStrategy}
+                  >
+                    {board.lists?.sort((a, b) => a.position - b.position)
+                      .map((boardList) => (
+                        <BoardList boardList={boardList} key={boardList.boardListId}/>
+                      ))}
+                  </SortableContext>
+                )}
+                <DragOverlay dropAnimation={{
+                  duration: 300,
+                  easing: 'cubic-bezier(0.2, 0, 0, 1)',
+                }}>
+                  {activeBoardList && (
+                    <BoardListOverlay boardList={activeBoardList}/>
+                  )}
+                </DragOverlay>
+                <CreateNewBoardList/>
+              </div>
+
+
+              {focusedListItem && !isEditingStatusTypes && (
+                <ScreenPanelOverlay>
+                  <ListItemSettings listItem={focusedListItem} closeSettings={() => setFocusedListItem(null)}
+                                    board={board}/>
+                </ScreenPanelOverlay>
+              )}
+
+              {isEditingStatusTypes && (
+                <ScreenPanelOverlay>
+                  <EditStatusTypes statusTypes={board.statusTypes} close={() => setIsEditingStatusTypes(false)}/>
+                </ScreenPanelOverlay>
+              )}
+
+
             </div>
-            <hr className="text-secondary"/>
-            <div className="w-full h-full flex gap-8 overflow-x-scroll overflow-y-hidden">
-              {board.lists?.map((boardList) => (
-                <BoardList boardList={boardList} key={boardList.boardListId} />
-              ))}
-              <CreateNewBoardList/>
-            </div>
-
-
-            {focusedListItem && !isEditingStatusTypes && (
-              <ScreenPanelOverlay>
-                <ListItemSettings listItem={focusedListItem} closeSettings={() => setFocusedListItem(null)} board={board} />
-              </ScreenPanelOverlay>
-            )}
-
-            {isEditingStatusTypes && (
-              <ScreenPanelOverlay>
-                <EditStatusTypes statusTypes={board.statusTypes} close={() => setIsEditingStatusTypes(false)} />
-              </ScreenPanelOverlay>
-            )}
-
-
-          </div>
-        )}
-      </div>
+          )}
+        </div>
+      </DndContext>
     </AuthProvider>
-  );
+  )
 };
 
 export default Page;
--- frontend/src/components/board/BoardList.tsx
+++ frontend/src/components/board/BoardList.tsx
@@ -2,19 +2,34 @@
 import React, {FormEvent, useState} from 'react';
 import {RiAddLine} from "@remixicon/react";
 import {useWebSocketStore} from "@/stores/websocket.store";
-import {BoardList as BoardListType} from "@/types/board.types";
+import {Board, BoardList as BoardListType} from "@/types/board.types";
 import LoadingSpinner from "@/components/LoadingSpinner";
 import ListItem from "@/components/board/list-item/ListItem";
 import CreateNewListItem from "@/components/board/list-item/CreateNewListItem";
+import {useSortable} from "@dnd-kit/sortable";
+import {CSS} from "@dnd-kit/utilities";
 
 export const BoardList = ({boardList}: {
   boardList: BoardListType;
 }) => {
 
+
+  const {attributes, listeners, setNodeRef, transition, transform} = useSortable({
+    id: boardList.boardListId,
+  });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition
+  };
+
   return (
     <div
-      className="min-w-72 min-h-24 h-fit max-h-full flex flex-col p-2 gap-4 bg-secondary-dark shadow-xl hover:shadow-2xl rounded-md border-secondary-dark border hover:border-accent duration-200 cursor-pointer">
-      <h2 className="text-white font-semibold text-lg">{boardList.name}</h2>
+      className="min-w-72 min-h-24 h-fit max-h-full flex flex-col p-2 gap-4 bg-secondary-dark shadow-xl hover:shadow-2xl rounded-md border-secondary-dark border hover:border-accent duration-200 " style={style}>
+
+      <div className="bg-green-500 cursor-pointer" ref={setNodeRef} {...attributes} {...listeners}>
+        <h2 className="text-white font-semibold text-lg">{boardList.name}</h2>
+      </div>
 
       {boardList.listItems?.sort((a, b) => a.position - b.position)
         .map((listItem) => (
@@ -62,6 +77,28 @@ export const CreateNewBoardList = () => {
           </button>
         </div>
       </form>
+    </div>
+  )
+}
+
+export const BoardListOverlay = ({boardList}: {
+  boardList: BoardListType;
+}) => {
+  return (
+    <div
+      className="min-w-72 min-h-24 h-fit max-h-full flex bg-secondary-dark opacity-50 flex-col p-2 gap-4 bg-black shadow-xl hover:shadow-2xl rounded-md border-secondary-dark border hover:border-accent duration-200 ">
+
+      <div className="bg-green-500 cursor-pointer">
+        <h2 className="text-white font-semibold text-lg">{boardList.name}</h2>
+      </div>
+
+      {boardList.listItems?.sort((a, b) => a.position - b.position)
+        .map((listItem) => (
+          <ListItem key={listItem.listItemId} listItem={listItem}/>
+        ))}
+
+      <CreateNewListItem boardList={boardList}/>
+
     </div>
   )
 }
