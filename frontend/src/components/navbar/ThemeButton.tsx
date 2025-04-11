8e0fbfe693642f6dc038265a1feb457ac24fe18e
Tyler Wade <vaxonreal@gmail.com>
2025-04-11 16:32:32 -0400

    Theme switch system updated.

--- frontend/src/components/navbar/ThemeButton.tsx
+++ frontend/src/components/navbar/ThemeButton.tsx
@@ -5,38 +5,48 @@ import Label from "@/components/Label";
 
 const ThemeButton = () => {
 
-  const [isDarkMode, setIsDarkMode] = useState<boolean>();
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(true);
 
   function toggleTheme() {
-    if (isDarkMode) {
-      document.documentElement.setAttribute('data-theme', 'light');
-      localStorage.setItem('theme', 'light');
-      setIsDarkMode(false);
-    } else {
+    const newMode = !isDarkMode;
+    if (newMode) {
       document.documentElement.setAttribute('data-theme', 'dark');
-      localStorage.setItem('theme', 'dark');
-      setIsDarkMode(true);
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.setAttribute('data-theme', 'light');
+      document.documentElement.classList.remove('dark');
     }
+
+    localStorage.setItem('theme', newMode ? 'dark' : 'light');
+    setIsDarkMode(newMode);
   }
 
   useEffect(() => {
-    const savedTheme = localStorage.getItem("theme");
-    setIsDarkMode(savedTheme === 'dark');
+    const savedTheme = localStorage.getItem('theme')
+      || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
 
-    if (savedTheme) {
-      document.documentElement.setAttribute('data-theme', savedTheme);
+    const isDark = savedTheme === 'dark';
+    setIsDarkMode(isDark);
+
+    if (isDark) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.setAttribute('data-theme', 'light');
+      document.documentElement.classList.remove('dark');
     }
+
   }, []);
 
   return (
     <div className="relative group">
       <button onClick={toggleTheme} className="hover-btn">
         {isDarkMode
-          ? <RiSunFill />
-          : <RiMoonClearFill />
+          ? <RiSunFill/>
+          : <RiMoonClearFill/>
         }
       </button>
-      <Label text={"Toggle Theme"} />
+      <Label text={"Toggle Theme"}/>
     </div>
   );
 };
